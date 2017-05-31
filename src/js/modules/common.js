c8980b9cbe13e6212dcd28bcc332a40046c2fcda
vdyshkant <yong.knox@yandex.ru>
2017-05-31 10:39:29 +0300

    p-promo-item:: minor edits, breadcrumbs js

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -161,4 +161,42 @@ RESET
 })(jQuery); // eof autoFunction
 /* eof ^^^ */
 
+(function($) {
+  if ((window.matchMedia("(max-width: 1023px)").matches)) {
+
+    var target = $('.breadcrumbs-link:last');
+    var cutText = target.attr( "data-original" );
+    var result = start_and_end(cutText);
+    target.html( result );
+
+    function start_and_end(str) {
+      if (str.length > 30) {
+        return str.substr(0, 5) + '...' + str.substr(str.length-5, str.length);
+      }
+      return str;
+    }
+
+  }
+  if ((window.matchMedia("(min-width: 1024px)").matches)) {
+
+    var target = $('.breadcrumbs-link:last');
+    var cutText = target.attr( "data-original" );
+    var result = start_and_end(cutText);
+    target.html( result );
+
+    function start_and_end(str) {
+      if (str.length > 30) {
+        return str.substr(0, 5) + '...' + str.substr(str.length-5, str.length);
+      }
+      return str;
+    }
+
+  }
+
+
+
+
+})(jQuery); // eof autoFunction
+
+
 // burger:
