c88afb6fbe219c1860067a0e0fcdd77fc789811b
vdyshkant <yong.knox@yandex.ru>
2017-06-01 20:44:08 +0300

    p-question -- made adaptation, layout, beta ver

--- src/js/modules/common.js
+++ src/js/modules/common.js
@@ -167,7 +167,7 @@ RESET
 (function($) {
   if ((window.matchMedia("(max-width: 1023px)").matches)) {
 
-    var target = $('.breadcrumbs-link:last');
+    var target = $('.breadcrumbs-item .breadcrumbs-link:last');
     var cutText = target.attr( "data-original" );
     var result = start_and_end(cutText);
     target.html( result );
@@ -182,7 +182,7 @@ RESET
   }
   if ((window.matchMedia("(min-width: 1024px)").matches)) {
 
-    var target = $('.breadcrumbs-link:last');
+    var target = $('.breadcrumbs-item .breadcrumbs-link:last');
     var cutText = target.attr( "data-original" );
     var result = start_and_end(cutText);
     target.html( result );
